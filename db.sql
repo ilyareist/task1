13bf20d0ae2f59411ae060e05d6a2ad66a10a9b2
Ilia <reistlin87@yandex.ru>
2019-11-04 22:51:57 +0200

    update gitigonre

--- db.sql
+++ db.sql
@@ -1,3 +1,29 @@
+CREATE TABLE public.accounts (
+    id character varying(255) NOT NULL,
+    country character varying(50) NOT NULL,
+    city character varying(50) NOT NULL,
+    balance numeric(16,4) NOT NULL,
+    currency character varying(3) NOT NULL,
+    deleted boolean NOT NULL
+);
+
+ALTER TABLE public.accounts OWNER TO postgres;
+
+CREATE TABLE public.payments (
+    id character varying(36) NOT NULL,
+    account character varying(255),
+    amount numeric(16,4) NOT NULL,
+    to_account character varying(255),
+    from_account character varying(255),
+    direction character varying(16) NOT NULL,
+    deleted boolean NOT NULL
+);
+
+
+ALTER TABLE public.payments OWNER TO postgres;
+
+
+
 CREATE INDEX payments_account_direction_index ON payments (account, direction);
 
 CREATE OR REPLACE VIEW accounts_view AS
