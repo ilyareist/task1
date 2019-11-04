33260d7b9c5986f2c6b8c30b4967e6c67ca6072b
Ilia <reistlin87@yandex.ru>
2019-11-04 23:30:12 +0200

    changed db script

--- db.sql
+++ db.sql
@@ -1,3 +1,56 @@
+--
+-- PostgreSQL database dump
+--
+
+CREATE TABLE public.accounts (
+    id character varying(255) NOT NULL,
+    country character varying(50) NOT NULL,
+    city character varying(50) NOT NULL,
+    balance numeric(16,4) NOT NULL,
+    currency character varying(3) NOT NULL,
+    deleted boolean NOT NULL
+);
+
+
+ALTER TABLE public.accounts OWNER TO postgres;
+
+--
+-- Name: accounts accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+CREATE TABLE public.payments (
+    id character varying(36) NOT NULL,
+    account character varying(255),
+    amount numeric(16,4) NOT NULL,
+    to_account character varying(255),
+    from_account character varying(255),
+    direction character varying(16) NOT NULL,
+    deleted boolean NOT NULL
+);
+
+
+ALTER TABLE public.payments OWNER TO postgres;
+
+--
+-- Name: payments payments_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.payments
+    ADD CONSTRAINT payments_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
 CREATE OR REPLACE VIEW accounts_view AS
 SELECT A.id,
        A.balance
